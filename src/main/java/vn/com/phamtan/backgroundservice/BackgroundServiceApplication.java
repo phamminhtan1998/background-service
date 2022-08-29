4945a4ebb52c72c5ad0ecc794dc9a4eac6db1a30
phamminhtan <phamtan1ltt1998@gmail.com>
2022-08-29 20:38:12 +0700

    first commit

---
+++ src/main/java/vn/com/phamtan/backgroundservice/BackgroundServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.com.phamtan.backgroundservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackgroundServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackgroundServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/com/phamtan/backgroundservice/BackgroundServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.phamtan.backgroundservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackgroundServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
